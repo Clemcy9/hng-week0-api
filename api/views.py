b6c1d7697ab3d53c5247ab039a4762d2c1f9706e
clement <clemcy9@gmail.com>
2025-01-29 13:10:01 +0100

    created view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,20 @@
+from datetime import datetime
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+
+def user_dated_details(request):
+    return Response({
+        "email": "clemcy9@gmail.com",
+        "current_datetime": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
+        "github_url": ""
+    })
+
+{
+  "email": "your-email@example.com",
+  "current_datetime": "2025-01-30T09:30:00Z",
+  "github_url": "<https://github.com/yourusername/your-repo>"
+}
