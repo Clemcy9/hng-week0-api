e139aab93c8f2ebb5ec8dcbffe44734b437e752f
clement <clemcy9@gmail.com>
2025-02-03 12:07:39 +0100

    written functions to calcultate even, odd, sum, is_prime, is_perfect for a given number, also written an api view to return as required

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import user_dated_details
+from .views import user_dated_details, NumberAPIView
 
 
 urlpatterns = [
     path('user-details/', user_dated_details, name='user_details'),
+    path('classify-number/<str:num>', NumberAPIView.as_view(), name = 'number-api'),
 ]
--- api/views.py
+++ api/views.py
@@ -4,8 +4,17 @@ from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework import status
+import requests
 
+
+
+# {
+#   "email": "your-email@example.com",
+#   "current_datetime": "2025-01-30T09:30:00Z",
+#   "github_url": "<https://github.com/yourusername/your-repo>"
+# }
 @api_view(['GET'])
 def user_dated_details(request):
     return Response({
@@ -14,8 +23,73 @@ def user_dated_details(request):
         "github_url": "https://github.com/Clemcy9/hng-week0-api"
     })
 
+
+
+
+
 # {
-#   "email": "your-email@example.com",
-#   "current_datetime": "2025-01-30T09:30:00Z",
-#   "github_url": "<https://github.com/yourusername/your-repo>"
+#     "number": 371,
+#     "is_prime": false,
+#     "is_perfect": false,
+#     "properties": ["armstrong", "odd"],
+#     "digit_sum": 11,  // sum of its digits
+#     "fun_fact": "371 is an Armstrong number because 3^3 + 7^3 + 1^3 = 371" //gotten from the numbers API
 # }
+# Required JSON Response Format (400 Bad Request)
+# {
+#     "number": "alphabet",
+#     "error": true
+# }
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2,int(n ** 0.5) + 1): # check from 2 to the sqrt of number, a form of mirroring
+        if n % i == 0:
+            return False
+    return True
+
+def is_perfect(n):
+    divisor_sum = sum(i for i in range(1,n) if n % i == 0) # sum nums from 1 to n-1 only if n/num =0
+    return divisor_sum == n
+
+def sum_of_digits(n):
+    return eval('+'.join(n))
+
+def is_armstrong(n:str):
+    digits = [int(digit) for digit in n]
+    num_digits = len(digits)
+    return n == sum(digit ** num_digits for digit in digits)
+
+def is_even_odd(n):
+    if int(n) % 2 == 0:
+        return 'even'
+    else:
+        return 'odd'
+
+class NumberAPIView(APIView):
+    def get(self, request, num):
+        if not num.isdigit():
+            return Response({
+                "number": "alphabet",
+                "error": True
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        num_properties = []
+        if is_armstrong(num):
+            num_properties.append('armstrong')
+        num_properties.append(is_even_odd(num))
+        print(f'this is numberProp: {num_properties}')
+
+        data = {
+            "number": int(num),
+            "is_prime": is_prime(int(num)),
+            "is_perfect": is_perfect(int(num)),
+            "properties": num_properties,
+            "digit_sum": sum_of_digits(num),
+            "fun_fact": requests.get(f'http://numbersapi.com/{int(num)}').text
+        }
+
+        return Response(data=data, status=status.HTTP_200_OK)
+
+
